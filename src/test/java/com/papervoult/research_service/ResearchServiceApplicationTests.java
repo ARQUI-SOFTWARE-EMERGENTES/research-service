a31478c30bbe0aa83b48d41b22cd257107164841
Jonatan Curi <u201912404@upc.edu.pe>
2024-06-03 10:57:33 -0500

    first commit

---
+++ src/test/java/com/papervoult/research_service/ResearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papervoult.research_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
